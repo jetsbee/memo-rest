08d4db01bda7850b1e98f06da02ad1fd8bdcc0c6
JaeseobHwang <jetsbee@gmail.com>
2016-08-21 20:49:59 +0900

    JPA, Package structure 1. Accessing JPA Data with REST ref. https://spring.io/guides/gs/accessing-data-rest/ 2. Structuring code ref. http://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-structuring-your-code.html

--- memo-rest/src/main/java/com/jetsbee/memo/Application.java
+++ memo-rest/src/main/java/com/jetsbee/memo/Application.java
@@ -1,4 +1,4 @@
-package memo;
+package com.jetsbee.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- memo-rest/src/main/java/com/jetsbee/memo/web/SampleController.java
+++ memo-rest/src/main/java/com/jetsbee/memo/web/SampleController.java
@@ -1,4 +1,4 @@
-package memo;
+package com.jetsbee.memo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-	@RequestMapping("/sample")
+	@RequestMapping("/")
 	public String sampleIt(){
 		return "Hello! Welcome to Spring Boot Sample. ";
 	}
